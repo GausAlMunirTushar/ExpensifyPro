33b34b6840b691fb79db59d19a8bba81d3f2f44f
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-16 23:12:06 +0600

    home page design and route setup completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-function App() {
+import React from 'react';
+import './assets/css/style.css'
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+const App = () => {
   return (
-   
-  )
-}
-
-export default App
+   <>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+    </Routes>
+   </>
+  );
+};
+export default App;
---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../assets/css/style.css'
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light py-3 padding'>
+        <h5 className="text-center">All rights reserved &copy; Gaus Al Munir Tushar</h5>
+    </div>
+  )
+}
+
+export default Footer
