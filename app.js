bde8c4f04e95250c562cf920edafe037e3a0bb5b
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-16 20:55:02 +0600

    database connectoin established

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const connectDB = require('./config/database');
 
 // config dot env file
 dotenv.config();
+
+// Database Connection
+connectDB();
 
 // Rest Object
 const app = express();
