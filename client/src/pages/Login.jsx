d01aaca65b8fad379923818ebea51ab43859975d
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-17 21:08:58 +0600

    registration and login completed funtionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import {Form, Input} from 'antd'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Form, Input, message} from 'antd'
+import { Link, useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import Spinner from '../components/Spinner'
 const Login = () => {
-    const submitHandler = (values) => {
-        console.log(values)
+    const uri = 'https://amused-snaps-hen.cyclic.app'
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+    const submitHandler = async (values) => {
+        try {
+            setLoading(true)
+            const {data} = await axios.post(uri + '/users/login', values)
+            setLoading(false)
+            message.success('Login Success')
+            localStorage.setItem('user', JSON.stringify({...data.user, password: ''}))
+            navigate('/')
+        } catch (error) {
+            setLoading(false)
+            message.error('something went wrong')
+        }
     }
   return (
     <>
         <div className="register">
+            {loading && <Spinner/>}
             <Form layout='vertical' onFinish={submitHandler}>
                     <h1 className='text-center'>Login</h1>
                     <Form.Item label='Email' name='email'>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import {Form, Input} from 'antd'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Form, Input, message} from 'antd'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Spinner from '../components/Spinner'
 const Register = () => {
-    const submitHandler = (values) => {
-        console.log(values)
+    const uri = 'https://amused-snaps-hen.cyclic.app'
+    const nagivate = useNavigate()
+    const [loading, setLoading] = useState(false)
+    const submitHandler = async (values) => {
+        try {
+            setLoading(true)
+            await axios.post(uri +'/users/register', values)
+            message.success('Registration Successfull')
+            setLoading(false)
+            nagivate('/login')
+        } catch (error) {
+            setLoading(false)
+            message.error('something went wrong')
+        }
     }
   return (
     <>
         <div className="register">
+            {loading && <Spinner/>}
             <Form layout='vertical' onFinish={submitHandler}>
                 <h1 className='text-center'>Register</h1>
                 <Form.Item label='Name' name='name'>
