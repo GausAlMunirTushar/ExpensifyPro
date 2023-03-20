8b88bea114903a191cdd2b8df1dee2ef1c49338f
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-20 21:17:15 +0600

    filter date and type

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,71 @@
-import React, {useState} from 'react'
-import {Form, Input, Modal, Select, message} from 'antd'
+import React, {useState, useEffect} from 'react'
+import {Form, Input, Modal, Select, Table, message, DatePicker} from 'antd'
 import Layout from '../components/Layout/Layout'
 import axios from 'axios'
 import Spinner from '../components/Spinner'
-
+import moment from 'moment'
+const {RangePicker} = DatePicker;
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false) 
   const [loading, setLoading] = useState(false)
+  const [alltransaction, setAlltransaction ] = useState([])
+  const [frequency, setFrequency] = useState('7')
+  const [selectedDate, setSelectedDate] = useState([])
+  const [type, setType] = useState('all')
+  //table data
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "date",
+      render: (text) => <span>{moment(text).format('YYYY-MM-DD')}</span>
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount"
+    },
+    {
+      title: "Type",
+      dataIndex: "type"
+    },
+    {
+      title: "Category",
+      dataIndex: "category"
+    },
+    {
+      title: "Reference",
+      dataIndex: "reference"
+    },
+    {
+      title: "Actions"
+    },
+  ]
+  // get all Transactions
+  const getAllTransaction = async () => {
+    const uri = 'https://amused-snaps-hen.cyclic.app'
+    try {
+      const user = JSON.parse(localStorage.getItem('user'))
+      setLoading(true)
+      const res = await axios.post(uri +'/transactions/get-transaction', {
+        userid: user._id, 
+        frequency,
+        selectedDate,
+        type
+      })
+      setLoading(false)
+      setAlltransaction(res.data)
+      console.log(res.data)
+    } catch (error) {
+      console.log(error)
+      message.error('Ftech issue with tranction')
+    }
+  }
+  // useEffect Hook
+  useEffect(() => {
+    getAllTransaction();
+  },[frequency, selectedDate, type])
   // from handling
   const handleSumbit =  async(values) => {
-          const uri = 'https://amused-snaps-hen.cyclic.app'
+    const uri = 'https://amused-snaps-hen.cyclic.app'
       try {
           const user = JSON.parse(localStorage.getItem('user'))
           setLoading(true)
@@ -26,12 +82,32 @@ const HomePage = () => {
     <Layout>
       {loading && <Spinner/>}
           <div className="filters">
-              <div>Range filter</div>
+              <div>
+                <h6>Select Frequency</h6>
+                <Select value={frequency} onChange={(values) => setFrequency(values)}>
+                  <Select.Option value="7">Last 1 Week</Select.Option>
+                  <Select.Option value="30">Last 1 Month</Select.Option>
+                  <Select.Option value="365">Last 1 Year</Select.Option>
+                  <Select.Option value="custom">Custom</Select.Option>
+                </Select>
+                {frequency === 'custom' && <RangePicker value={selectedDate} onChange={(values) => {values}}/>}
+              </div>
+              <div>
+                <h6>Select Type</h6>
+                <Select value={type} onChange={(values) => setType(values)}>
+                  <Select.Option value="all">All</Select.Option>
+                  <Select.Option value="income">Income</Select.Option>
+                  <Select.Option value="expense">Expense</Select.Option>
+                </Select>
+                {frequency === 'custom' && <RangePicker value={selectedDate} onChange={(values) => {values}}/>}
+              </div>
               <div>
                 <button className='btn btn-primary' onClick={() => setShowModal(true)}>Add New</button>
               </div>
           </div>
           <div className="content">
+            <Table columns={columns} dataSource={alltransaction}/>
+          </div>
               <Modal 
               title="Add Transaction" 
               open={showModal} 
@@ -71,7 +147,6 @@ const HomePage = () => {
                       </div>
                   </Form>
               </Modal>
-          </div>
     </Layout>
   )
 }
