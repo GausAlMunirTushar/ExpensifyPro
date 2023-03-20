9b5f614e9994628d5cb6b021a8d291c90524a227
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-20 21:16:45 +0600

    date and type find

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,5 +1,5 @@
 const transactionModel = require('../models/transactionModel')
-
+const moment = require('moment')
 const addTransaction = async (req, res) => {
     try {
         const reqBody = req.body;
@@ -14,7 +14,20 @@ const addTransaction = async (req, res) => {
 
 const getAllTransaction = async (req, res) => {
     try {
-        const transaction = await transactionModel.find({userid: req.body.userid})
+        const {frequency, selectedDate, type} = req.body
+        const transaction = await transactionModel.find({
+            ...(frequency !== 'custom' ? {
+                date: {
+                    $gt: moment().subtract(Number(frequency), 'd').toDate()
+                },
+            } : {
+                $gte: selectedDate[0],
+                $lte: selectedDate[1]
+            }),
+            userid: req.body.userid,
+            ...(type !== 'all' && {type})
+        })
+            
         res.status(200).json(transaction);
     } catch (error) {
         console.log(error)
